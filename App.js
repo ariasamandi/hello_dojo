e26f3275a607d3a858ad7fae244d6d4018581c16
Aria Samandi <ariasamandi@gmail.com>
2019-04-19 11:12:24 -0700

    Hello Dojo

---
+++ App.js
@@ -0,0 +1,3 @@
+const Hello = React.createElement('div', null, React.createElement('h1', null, 'Hello Dojo!'), React.createElement('h2', null, 'Things to do:'), React.createElement('ul', null, React.createElement('li', null, 'Learn React'), React.createElement('li', null, 'Climb Mt. Everest'), React.createElement('li', null, 'Run a Marahton'), React.createElement('li', null, 'Feed the dogs')));
+    
+	ReactDOM.render(Hello, document.getElementById('app'));
